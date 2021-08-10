94ced129b2ca96f48dea6f0c091e23838a326c75
Iric <iricbing@gmail.com>
2021-08-10 18:06:25 +0800

    增加UWP笔记

---
+++ docs/Windows/桌面应用程序开发/UWP/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: 'UWP',
+  path: '/Windows/桌面应用程序开发/UWP/'
+};
--- docs/Windows/桌面应用程序开发/index.js
+++ docs/Windows/桌面应用程序开发/index.js
@@ -1,5 +1,6 @@
 const winForm = require('./WinForm');
 const wpf = require('./WPF');
+const uwp = require('./UWP');
 
 module.exports = {
   title: '桌面应用程序开发',
@@ -9,6 +10,7 @@ module.exports = {
       path: '/Windows/桌面应用程序开发/方案对比'
     },
     winForm,
-    wpf
+    wpf,
+    uwp
   ]
 };
