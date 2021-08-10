4d1c3f3e99c4fa03d8d4dcc854eb9f3931f6f2b4
Iric <iricbing@gmail.com>
2021-08-10 09:28:28 +0800

    完成基本框架

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // plugins:[
+  //   [
+  //     '@vuepress/last-updated',
+  //     {
+  //       transformer: (timestamp, lang) => {
+  //         const dayjs = require('dayjs')
+  //         dayjs.locale(lang)
+  //         return dayjs(timestamp).format('YYYY-MM-DD HH:mm:ss')
+  //       }
+  //     }
+  //   ]
+  // ],
+  title: 'Iric',
+  description: '个人文档',
+  themeConfig: {
+    logo: '/logo.jpg',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '开始', link: '/guide/' },
+      { text: 'GitHub', link: 'https://github.com/IricBing/megrez' },
+    ],
+    lastUpdated: '上次更新'
+  }
+}
