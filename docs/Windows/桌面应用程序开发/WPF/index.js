529d46bde54fe55aa0f31a059185f014710539de
IricBing95 <iricbing@gmail.com>
2021-08-11 11:38:38 +0800

    增加图片测试

--- docs/Windows/桌面应用程序开发/WPF/index.js
+++ docs/Windows/桌面应用程序开发/WPF/index.js
@@ -1,7 +1,8 @@
 const ui = require('./UI框架');
+const faq = require('./填坑手册');
 
 module.exports = {
   title: 'WPF',
   path: '/Windows/桌面应用程序开发/WPF/',
-  children: [ui]
+  children: [faq, ui]
 };
---
+++ docs/Windows/桌面应用程序开发/WPF/填坑手册/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '填坑手册',
+  children:[
+    {
+      title:'找不到.NETFramework,Version=xx的引用程序集',
+      path:'/Windows/桌面应用程序开发/WPF/填坑手册/找不到.NetFramework的引用程序集'
+    }
+  ]
+};
