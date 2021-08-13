5326a3572ca4e757e522ef474ab699b754b8d3cb
Iric <iricbing@gmail.com>
2021-08-13 09:36:51 +0800

    增加vuepress上次更新时间配置笔记

--- docs/Web/vuepress/项目配置/index.js
+++ docs/Web/vuepress/项目配置/index.js
@@ -4,6 +4,10 @@ module.exports = {
     {
       title: '图片资源配置',
       path: '/Web/vuepress/项目配置/图片资源配置'
+    },
+    {
+      title: '上次更新时间配置',
+      path: '/Web/vuepress/项目配置/上次更新时间配置'
     }
   ]
-}
+};
