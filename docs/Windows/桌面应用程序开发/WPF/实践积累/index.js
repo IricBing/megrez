f0b28d8b095ad589571857ca4685b516c312baf9
IricBing95 <iricbing@gmail.com>
2021-08-16 23:00:15 +0800

    增加HandyControl自定义按钮图标笔记

--- docs/Windows/桌面应用程序开发/WPF/实践积累/index.js
+++ docs/Windows/桌面应用程序开发/WPF/实践积累/index.js
@@ -2,6 +2,10 @@ module.exports = {
   title: '实践积累',
   children: [
     {
+      title: '使用 SVG',
+      path:'/Windows/桌面应用程序开发/WPF/实践积累/使用svg'
+    },
+    {
       title: '可视化大屏',
       path:'/Windows/桌面应用程序开发/WPF/实践积累/可视化大屏'
     }
