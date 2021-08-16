0720c18228f7236c581f253c84aae4fd968eb93c
IricBing95 <iricbing@gmail.com>
2021-08-16 15:13:48 +0800

    修复HandyControl vs插件笔记bug

--- docs/Windows/桌面应用程序开发/WPF/UI框架/HandyControl/环境篇/index.js
+++ docs/Windows/桌面应用程序开发/WPF/UI框架/HandyControl/环境篇/index.js
@@ -1,10 +1,9 @@
 module.exports = {
   title: '环境篇',
-  path: '/Windows/桌面应用程序开发/WPF/UI框架/HandyControl/',
   children: [
     {
       title: 'vs插件',
-      path: '/Windows/桌面应用程序开发/WPF/UI框架/HandyControl/vs插件'
+      path: '/Windows/桌面应用程序开发/WPF/UI框架/HandyControl/环境篇/vs插件'
     }
   ]
 };
