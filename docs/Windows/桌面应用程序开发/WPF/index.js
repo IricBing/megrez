a04c33ee28c5ecee1f1678eb3d3c85991130eff0
Iric <iricbing@gmail.com>
2021-08-16 08:53:31 +0800

    增加go可视化大屏笔记

--- docs/Windows/桌面应用程序开发/WPF/index.js
+++ docs/Windows/桌面应用程序开发/WPF/index.js
@@ -1,8 +1,9 @@
 const ui = require('./UI框架');
 const faq = require('./填坑手册');
+const experience = require('./实践积累');
 
 module.exports = {
   title: 'WPF',
   path: '/Windows/桌面应用程序开发/WPF/',
-  children: [faq, ui]
+  children: [faq, ui, experience]
 };
---
+++ docs/Windows/桌面应用程序开发/WPF/实践积累/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '实践积累',
+  children: [
+    {
+      title: '可视化大屏',
+      path:'/Windows/桌面应用程序开发/WPF/实践积累/可视化大屏'
+    }
+  ]
+};
