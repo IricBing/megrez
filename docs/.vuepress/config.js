3b352e7d54c1865e1405dfeaaf74bf59507c9a34
IricBing95 <iricbing@gmail.com>
2021-08-19 11:24:51 +0800

    增加vuepree宠物猫扩展

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,6 +14,7 @@ module.exports = {
     }
   },
   plugins: [
+    'vuepress-plugin-cat',
     [
       '@vuepress/last-updated',
       {
--- docs/Web/vuepress/扩展插件/index.js
+++ docs/Web/vuepress/扩展插件/index.js
@@ -4,6 +4,10 @@ module.exports = {
     {
       title: '复制弹窗插件',
       path: '/Web/vuepress/扩展插件/复制弹窗插件'
+    },
+    {
+      title: '集成宠物猫',
+      path: '/Web/vuepress/扩展插件/宠物猫'
     }
   ]
 };
