7f9fc5be700199254834d37d703b0590f4ea9ed5
IricBing95 <iricbing@gmail.com>
2021-08-21 10:21:27 +0800

    增加windows桌面应用程序HPSocket的安装方式

--- docs/Windows/桌面应用程序开发/HPSocket/index.js
+++ docs/Windows/桌面应用程序开发/HPSocket/index.js
@@ -1,4 +1,7 @@
+const experience = require('./实践篇');
+
 module.exports = {
   title: 'HPSocket',
-  path: '/Windows/桌面应用程序开发/HPSocket/'
+  path: '/Windows/桌面应用程序开发/HPSocket/',
+  children: [experience]
 };
---
+++ docs/Windows/桌面应用程序开发/HPSocket/实践篇/Index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '实践篇',
+  children: [
+    {
+      title: '安装',
+      path: '/Windows/桌面应用程序开发/HPSocket/实践篇/安装'
+    }
+  ]
+};
