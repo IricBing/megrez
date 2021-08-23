6fa9e41f0c1598cc41dd77e8ca2906fd59160056
Iric <iricbing@gmail.com>
2021-08-23 11:25:48 +0800

    增加vuepress图片资源配置笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,6 +14,12 @@ module.exports = {
       md.use(require('markdown-it-disable-url-encode'));
     }
   },
+  // head: [
+  //   // add jquert and fancybox
+  //   ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js' }],
+  //   ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.js' }],
+  //   ['link', { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.css' }]
+  // ],
   plugins: [
     'vuepress-plugin-cat',
     [
--- docs/Web/vuepress/项目配置/index.js
+++ docs/Web/vuepress/项目配置/index.js
@@ -6,6 +6,10 @@ module.exports = {
       path: '/Web/vuepress/项目配置/图片资源配置'
     },
     {
+      title: '图片放大配置',
+      path: '/Web/vuepress/项目配置/图片放大配置'
+    },
+    {
       title: '上次更新时间配置',
       path: '/Web/vuepress/项目配置/上次更新时间配置'
     }
