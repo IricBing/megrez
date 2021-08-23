2033eaa164b0a1bdfd5ee03f79fa9648e745edba
Iric <iricbing@gmail.com>
2021-08-23 18:59:55 +0800

    修改笔记路径bug

--- docs/运维/Nginx/错误处理/index.js
+++ docs/运维/Nginx/错误处理/index.js
@@ -3,23 +3,23 @@ module.exports = {
   children: [
     {
       title: '403错误处理',
-      path: '/运维/Nginx/配置篇/403错误处理'
+      path: '/运维/Nginx/错误处理/403错误处理'
     },
     {
       title: '413错误处理',
-      path: '/运维/Nginx/配置篇/413错误处理'
+      path: '/运维/Nginx/错误处理/413错误处理'
     },
     {
       title: '中文乱码问题',
-      path: '/运维/Nginx/配置篇/中文乱码'
+      path: '/运维/Nginx/错误处理/中文乱码'
     },
     {
       title: '字体跨域问题',
-      path: '/运维/Nginx/配置篇/字体跨域'
+      path: '/运维/Nginx/错误处理/字体跨域'
     },
     {
       title: 'SSL接收到一个超出最大准许长度的记录',
-      path: '/运维/Nginx/配置篇/SSL接收到一个超出最大准许长度的记录'
+      path: '/运维/Nginx/错误处理/SSL接收到一个超出最大准许长度的记录'
     }
   ]
 };
