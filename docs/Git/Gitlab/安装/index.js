9099d04e802f22bd89b34246452018da75da50cf
Iric <iricbing@gmail.com>
2021-08-25 18:31:00 +0800

    增加Docker安装笔记

--- docs/Git/Gitlab/安装/index.js
+++ docs/Git/Gitlab/安装/index.js
@@ -4,6 +4,14 @@ module.exports = {
     {
       title: 'Docker',
       path: '/Git/Gitlab/安装/Docker'
+    },
+    {
+      title: 'Docker Compose',
+      path: '/Git/Gitlab/安装/DockerCompose'
+    },
+    {
+      title: 'Ubuntu',
+      path: '/Git/Gitlab/安装/Ubuntu'
     }
   ]
 };
