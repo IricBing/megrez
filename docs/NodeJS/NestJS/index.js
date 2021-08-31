b1675389beb841863fcd04fd632cc5ee8f79d059
Iric <iricbing@gmail.com>
2021-08-31 15:52:22 +0800

    增加nestjs docker多阶段构建笔记

--- docs/NodeJS/NestJS/index.js
+++ docs/NodeJS/NestJS/index.js
@@ -1,7 +1,8 @@
 const config = require('./配置篇');
 const container = require('./容器镜像篇');
+const fillPitManual = require('./填坑手册');
 
 module.exports = {
   title: 'NestJS',
-  children: [config, container]
+  children: [config, container, fillPitManual]
 };
---
+++ docs/NodeJS/NestJS/填坑手册/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '填坑手册',
+  children: [
+    {
+      title: 'GRPC打包proto文件缺失',
+      path: '/NodeJS/NestJS/填坑手册/grpc打包proto文件缺失'
+    }
+  ]
+};
