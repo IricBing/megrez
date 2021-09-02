83aef43477c3363f38a2cdab564f2ca5ae012d4b
Iric <iricbing@gmail.com>
2021-09-02 18:54:53 +0800

    增加PostgreSQL加密函数

--- docs/数据库/PostgreSQL/基础篇/函数/index.js
+++ docs/数据库/PostgreSQL/基础篇/函数/index.js
@@ -1,7 +1,8 @@
 const mathFunction = require('./数学函数');
 const stringFunction = require('./字符串函数');
 const dateAndTimeFunction = require('./日期和时间函数');
-const systemInfoFunction = require('./系统信息函数')
+const systemInfoFunction = require('./系统信息函数');
+const encryptFunction = require('./加密函数');
 
 module.exports = {
   title: '函数',
@@ -13,6 +14,11 @@ module.exports = {
       title: '条件判断函数',
       path: '/数据库/PostgreSQL/基础篇/函数/条件判断函数'
     },
-    systemInfoFunction
+    systemInfoFunction,
+    encryptFunction,
+    {
+      title: '改变数据类型的函数',
+      path: '/数据库/PostgreSQL/基础篇/函数/改变数据类型的函数'
+    }
   ]
 };
---
+++ docs/数据库/PostgreSQL/基础篇/函数/加密函数/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: '加密函数',
+  children: [
+    {
+      title: '加密函数 MD5(str)',
+      path: '/数据库/PostgreSQL/基础篇/函数/加密函数/MD5加密函数'
+    },
+    {
+      title: '加密函数 ENCODE(str, pswd_str)',
+      path: '/数据库/PostgreSQL/基础篇/函数/加密函数/ENCODE加密函数'
+    },
+    {
+      title: '解密函数 DECODE(crypt_str, pswd_str)',
+      path: '/数据库/PostgreSQL/基础篇/函数/加密函数/DECODE解密函数'
+    }
+  ]
+};
