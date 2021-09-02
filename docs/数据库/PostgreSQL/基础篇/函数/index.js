79deeff331d3cdf2632cd27ff09158cd2366f831
Iric <iricbing@gmail.com>
2021-09-02 17:06:05 +0800

    增加PostgreSQL字符串函数笔记

--- docs/数据库/PostgreSQL/基础篇/函数/index.js
+++ docs/数据库/PostgreSQL/基础篇/函数/index.js
@@ -1,6 +1,7 @@
 const mathFunction = require('./数学函数');
+const stringFunction = require('./字符串函数');
 
 module.exports = {
   title: '函数',
-  children: [mathFunction]
+  children: [mathFunction, stringFunction]
 };
---
+++ docs/数据库/PostgreSQL/基础篇/函数/字符串函数/index.js
@@ -0,0 +1,49 @@
+module.exports = {
+  title: '字符串函数',
+  children: [
+    {
+      title: '计算字符串字符数函数(CHAR_LENGTH(str))和字符串长度函数(LENGTH(str))',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/字符数和长度函数'
+    },
+    {
+      title: '合并字符串函数CONCAT(s1, s2, ...)、CONCAT_WS(x, s1, s2, ...)',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/合并字符串函数'
+    },
+    {
+      title: '获取指定长度的字符串函数LEFT(s, n)和RIGHT(s, n)',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/获取指定长度的字符串'
+    },
+    {
+      title: '填充字符串函数LPAD(s1, len, s2)和RPAD(s1, len, s2)',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/填充字符串函数'
+    },
+    {
+      title: '删除空格函数LTRIM(s)、RTRIM(s)、TRIM(s)',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/删除空格函数'
+    },
+    {
+      title: '删除指定字符串函数 TRIM(s1 FROM s)',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/删除指定字符串函数'
+    },
+    {
+      title: '重复生成字符串函数 REPEAT(s, n)',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/重复生成字符串函数'
+    },
+    {
+      title: '替换函数REPLACE(s, s1, s2)',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/替换函数'
+    },
+    {
+      title: '获取子串函数 SUBSTRING(s, n, len)',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/获取子串函数'
+    },
+    {
+      title: '匹配子串开始位置的函数 POSITION(str1 IN str)',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/匹配子串开始位置的函数'
+    },
+    {
+      title: '字符串逆序函数 REVERSE(s)',
+      path: '/数据库/PostgreSQL/基础篇/函数/字符串函数/字符串逆序函数'
+    }
+  ]
+};
