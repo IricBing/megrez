be0053b723cfdc4d991301af70c963c47988bc20
Iric <iricbing@gmail.com>
2021-09-02 18:43:41 +0800

    增加PostgreSQL系统信息函数笔记

--- docs/数据库/PostgreSQL/基础篇/函数/index.js
+++ docs/数据库/PostgreSQL/基础篇/函数/index.js
@@ -1,8 +1,18 @@
 const mathFunction = require('./数学函数');
 const stringFunction = require('./字符串函数');
 const dateAndTimeFunction = require('./日期和时间函数');
+const systemInfoFunction = require('./系统信息函数')
 
 module.exports = {
   title: '函数',
-  children: [mathFunction, stringFunction, dateAndTimeFunction]
+  children: [
+    mathFunction,
+    stringFunction,
+    dateAndTimeFunction,
+    {
+      title: '条件判断函数',
+      path: '/数据库/PostgreSQL/基础篇/函数/条件判断函数'
+    },
+    systemInfoFunction
+  ]
 };
---
+++ docs/数据库/PostgreSQL/基础篇/函数/系统信息函数/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: '系统信息函数',
+  children: [
+    {
+      title: '获取版本号函数',
+      path: '/数据库/PostgreSQL/基础篇/函数/系统信息函数/获取版本号函数'
+    },
+    {
+      title: '获取用户名函数',
+      path: '/数据库/PostgreSQL/基础篇/函数/系统信息函数/获取用户名函数'
+    }
+  ]
+};
