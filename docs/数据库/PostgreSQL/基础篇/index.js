ac5a40dada08acfd766660e9e5ac7eb086b1f7b2
Iric <iricbing@gmail.com>
2021-09-02 16:42:30 +0800

    增加PostgreSQL运算符

--- docs/数据库/PostgreSQL/基础篇/index.js
+++ docs/数据库/PostgreSQL/基础篇/index.js
@@ -1,8 +1,9 @@
 const install = require('./安装');
 const baseOperate = require('./基本操作');
 const dataType = require('./数据类型');
+const operator = require('./运算符');
 
 module.exports = {
   title: '基础篇',
-  children: [install, baseOperate, dataType]
+  children: [install, baseOperate, dataType, operator]
 };
---
+++ docs/数据库/PostgreSQL/基础篇/运算符/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: '运算符',
+  children: [
+    {
+      title: '运算符优先级',
+      path: '/数据库/PostgreSQL/基础篇/运算符/运算符优先级'
+    },
+    {
+      title: '算数运算符',
+      path: '/数据库/PostgreSQL/基础篇/运算符/算数运算符'
+    },
+    {
+      title: '比较运算符',
+      path: '/数据库/PostgreSQL/基础篇/运算符/比较运算符'
+    },
+    {
+      title: '逻辑运算符',
+      path: '/数据库/PostgreSQL/基础篇/运算符/逻辑运算符'
+    },
+    {
+      title: '位操作运算符',
+      path: '/数据库/PostgreSQL/基础篇/运算符/位操作运算符'
+    }
+  ]
+};
