5240082cc74a0d43568bb9615a4079a825fd46cf
Iric <iricbing@gmail.com>
2021-09-02 16:11:20 +0800

    增加PostgreSQL数据类型笔记

--- docs/数据库/PostgreSQL/基础篇/index.js
+++ docs/数据库/PostgreSQL/基础篇/index.js
@@ -1,7 +1,8 @@
 const install = require('./安装');
 const baseOperate = require('./基本操作');
+const dataType = require('./数据类型');
 
 module.exports = {
   title: '基础篇',
-  children: [install, baseOperate]
+  children: [install, baseOperate, dataType]
 };
---
+++ docs/数据库/PostgreSQL/基础篇/数据类型/index.js
@@ -0,0 +1,37 @@
+module.exports = {
+  title: '数据类型',
+  children: [
+    {
+      title: '整数类型',
+      path: '/数据库/PostgreSQL/基础篇/数据类型/整数类型'
+    },
+    {
+      title: '浮点数类型',
+      path: '/数据库/PostgreSQL/基础篇/数据类型/浮点数类型'
+    },
+    {
+      title: '任意精度类型',
+      path: '/数据库/PostgreSQL/基础篇/数据类型/任意精度类型'
+    },
+    {
+      title: '日期与时间类型',
+      path: '/数据库/PostgreSQL/基础篇/数据类型/日期与时间类型'
+    },
+    {
+      title: '字符串类型',
+      path: '/数据库/PostgreSQL/基础篇/数据类型/字符串类型'
+    },
+    {
+      title: '二进制类型',
+      path: '/数据库/PostgreSQL/基础篇/数据类型/二进制类型'
+    },
+    {
+      title: '布尔类型',
+      path: '/数据库/PostgreSQL/基础篇/数据类型/布尔类型'
+    },
+    {
+      title: '数组类型',
+      path: '/数据库/PostgreSQL/基础篇/数据类型/数组类型'
+    }
+  ]
+};
