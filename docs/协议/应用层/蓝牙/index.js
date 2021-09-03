6b0e5974a2a994b5307a4b34dd03d8c94c2adfe4
Iric <iricbing@gmail.com>
2021-09-03 18:40:29 +0800

    增加蓝牙协议

--- docs/协议/应用层/蓝牙/index.js
+++ docs/协议/应用层/蓝牙/index.js
@@ -1,4 +1,13 @@
+const ble = require('./低功耗蓝牙');
+
 module.exports = {
   title: '蓝牙',
-  path: '/协议/应用层/蓝牙/'
+  path: '/协议/应用层/蓝牙/',
+  children: [
+    {
+      title: '版本',
+      path: '/协议/应用层/蓝牙/版本'
+    },
+    ble
+  ]
 };
---
+++ docs/协议/应用层/蓝牙/低功耗蓝牙/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: '低功耗蓝牙',
+  path: '/协议/应用层/蓝牙/低功耗蓝牙/',
+  children:[
+    {
+      title:'GATT',
+      path:'/协议/应用层/蓝牙/低功耗蓝牙/GATT'
+    },
+    {
+      title:'GAP',
+      path:'/协议/应用层/蓝牙/低功耗蓝牙/GAP'
+    },
+    {
+      title:'GAP广播数据',
+      path:'/协议/应用层/蓝牙/低功耗蓝牙/GAP广播数据'
+    },
+    {
+      title:'MTU',
+      path:'/协议/应用层/蓝牙/低功耗蓝牙/MTU'
+    }
+  ]
+};
