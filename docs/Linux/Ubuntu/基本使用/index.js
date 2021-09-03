76c9c6527905bb2b65fba066947c6b6a18aed79d
Iric <iricbing@gmail.com>
2021-09-03 09:24:11 +0800

    增加Ubuntu基本使用部分笔记

--- docs/Linux/Ubuntu/基本使用/index.js
+++ docs/Linux/Ubuntu/基本使用/index.js
@@ -2,5 +2,91 @@ const baseCmd = require('./基础命令');
 
 module.exports = {
   title: '基本使用',
-  children: [baseCmd]
+  children: [
+    baseCmd,
+    {
+      title: 'apt软件更新',
+      path: '/Linux/Ubuntu/基本使用/apt软件更新'
+    },
+    {
+      title: '添加用户',
+      path: '/Linux/Ubuntu/基本使用/添加用户'
+    },
+    {
+      title: '修改主机名称',
+      path: '/Linux/Ubuntu/基本使用/修改主机名称'
+    },
+    {
+      title: '防火墙',
+      path: '/Linux/Ubuntu/基本使用/防火墙'
+    },
+    {
+      title: '服务器传输',
+      path: '/Linux/Ubuntu/基本使用/服务器传输'
+    },
+    {
+      title: '软硬链接',
+      path: '/Linux/Ubuntu/基本使用/软硬链接'
+    },
+    {
+      title: '查看系统版本',
+      path: '/Linux/Ubuntu/基本使用/查看系统版本'
+    },
+    {
+      title: '查看CPU信息',
+      path: '/Linux/Ubuntu/基本使用/查看CPU信息'
+    },
+    {
+      title: '查看文件夹大小',
+      path: '/Linux/Ubuntu/基本使用/查看文件夹大小'
+    },
+    {
+      title: '查看串口',
+      path: '/Linux/Ubuntu/基本使用/查看串口'
+    },
+    {
+      title: '查看磁盘',
+      path: '/Linux/Ubuntu/基本使用/查看磁盘'
+    },
+    {
+      title: '挂载新磁盘',
+      path: '/Linux/Ubuntu/基本使用/挂载新磁盘'
+    },
+    {
+      title: '查看端口占用',
+      path: '/Linux/Ubuntu/基本使用/查看端口占用'
+    },
+    {
+      title: '查看端口监听',
+      path: '/Linux/Ubuntu/基本使用/查看端口监听'
+    },
+    {
+      title: '开机启动脚本',
+      path: '/Linux/Ubuntu/基本使用/开机启动脚本'
+    },
+    {
+      title: 'apt命令走代理',
+      path: '/Linux/Ubuntu/基本使用/apt命令走代理'
+    },
+    {
+      title: '时区设置',
+      path: '/Linux/Ubuntu/基本使用/时区设置'
+    },
+    {
+      title: '修改时间',
+      path: '/Linux/Ubuntu/基本使用/修改时间'
+    },
+    {
+      title: '网络时间同步',
+      path: '/Linux/Ubuntu/基本使用/网络时间同步'
+    },
+    {
+      title: '依赖库调整',
+      path: '/Linux/Ubuntu/基本使用/依赖库调整'
+    },
+    {
+      title: '中文支持',
+      path: '/Linux/Ubuntu/基本使用/中文支持'
+    }
+  ]
 };
