33c68df4058dbf2ce738d782bc526369905d93f8
Iric <iricbing@gmail.com>
2021-09-03 18:35:20 +0800

    增加Cat

---
+++ docs/协议/应用层/GRPC/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'GRPC',
+  path: '/协议/应用层/GRPC/',
+  children: [
+    {
+      title: 'RPC',
+      path: '/协议/应用层/GRPC/RPC'
+    }
+  ]
+};
--- docs/协议/应用层/index.js
+++ docs/协议/应用层/index.js
@@ -3,8 +3,9 @@ const mqtt = require('./MQTT');
 const tcpip = require('./TCPIP');
 const udp = require('./UDP');
 const bluetooth = require('./蓝牙');
+const grpc = require('./GRPC');
 
 module.exports = {
   title: '应用层',
-  children: [tcpip, http, mqtt, udp, bluetooth]
+  children: [tcpip, http, mqtt, udp, grpc, bluetooth]
 };
