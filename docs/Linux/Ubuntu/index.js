91f2571b6d48a93bbff8954fc9288872af95a82b
Iric <iricbing@gmail.com>
2021-09-03 09:42:39 +0800

    增加Ubuntu基础知识部分笔记

--- docs/Linux/Ubuntu/index.js
+++ docs/Linux/Ubuntu/index.js
@@ -1,7 +1,8 @@
 const v2004 = require('./20.04');
 const baseUsage = require('./基本使用');
+const baseKnowledge = require('./基础知识');
 
 module.exports = {
   title: 'Ubuntu',
-  children: [baseUsage, v2004]
+  children: [baseUsage, baseKnowledge, v2004]
 };
---
+++ docs/Linux/Ubuntu/基础知识/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: '基础知识',
+  children: [
+    {
+      title: '启动步骤',
+      path: '/Linux/Ubuntu/基础知识/启动步骤'
+    },
+    {
+      title: '运行级别',
+      path: '/Linux/Ubuntu/基础知识/运行级别'
+    },
+    {
+      title: 'update-rc.d',
+      path: '/Linux/Ubuntu/基础知识/update-rc.d'
+    },
+    {
+      title: 'LVM',
+      path: '/Linux/Ubuntu/基础知识/LVM'
+    },
+    {
+      title: '依赖库',
+      path: '/Linux/Ubuntu/基础知识/依赖库'
+    }
+  ]
+};
