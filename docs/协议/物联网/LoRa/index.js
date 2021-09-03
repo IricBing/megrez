77977bf1f25263d769542b5863b7807551bcad5b
Iric <iricbing@gmail.com>
2021-09-03 18:29:10 +0800

    增加LoRa笔记

--- docs/协议/物联网/LoRa/index.js
+++ docs/协议/物联网/LoRa/index.js
@@ -1,4 +1,18 @@
 module.exports = {
   title: 'LoRa',
-  path: '/协议/物联网/LoRa/'
+  path: '/协议/物联网/LoRa/',
+  children: [
+    {
+      title: '科普',
+      path: '/协议/物联网/LoRa/科普'
+    },
+    {
+      title: '终端节点的网络连接方式',
+      path: '/协议/物联网/LoRa/终端节点的网络连接方式'
+    },
+    {
+      title: '终端节点的三种工作模式, 用好 LoRa 终端的关键！',
+      path: '/协议/物联网/LoRa/终端节点的三种工作模式'
+    }
+  ]
 };
