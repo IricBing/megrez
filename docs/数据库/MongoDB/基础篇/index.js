a3061aa4ecafb86d873a6ea7f3fdfe1beda46963
Iric <iricbing@gmail.com>
2021-09-04 17:50:13 +0800

    增加MongoDB演变历程和主要特征笔记

--- docs/数据库/MongoDB/基础篇/index.js
+++ docs/数据库/MongoDB/基础篇/index.js
@@ -1,6 +1,16 @@
-const index = require('./索引')
+const index = require('./索引');
 
 module.exports = {
   title: '基础篇',
-  children: [index]
+  children: [
+    {
+      title: '主要特征',
+      path: '/数据库/MongoDB/基础篇/主要特征'
+    },
+    index,
+    {
+      title: '演变历程',
+      path: '/数据库/MongoDB/基础篇/演变历程'
+    }
+  ]
 };
