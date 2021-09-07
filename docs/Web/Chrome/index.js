7b2cdb8a8f0e47ed85326846aabe36055146da36
Iric <iricbing@gmail.com>
2021-09-07 09:12:34 +0800

    增加Chrome优秀扩展笔记

---
+++ docs/Web/Chrome/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: "Chrome",
+  children:[
+    {
+      title:'优秀扩展',
+      path:'/Web/Chrome/优秀扩展',
+    }
+  ]
+};
--- docs/Web/index.js
+++ docs/Web/index.js
@@ -4,8 +4,9 @@ const libs = require('./常用库');
 const experience = require('./实践积累');
 const vue2 = require('./Vue2');
 const uniApp = require('./uni-app');
+const chrome = require('./Chrome');
 
 module.exports = {
   title: 'Web',
-  children: [experience, libs, vue2, uniApp, miniProgram, vuepress]
+  children: [experience, libs, vue2, uniApp, miniProgram, vuepress, chrome]
 };
