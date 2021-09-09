67f6b4f644dd6efa079e3b89c4ddd6f6d0ec47ab
Iric <iricbing@gmail.com>
2021-09-09 12:27:39 +0800

    增加部分ubuntu 20.04软件安装笔记

--- docs/Linux/Ubuntu/20.04/index.js
+++ docs/Linux/Ubuntu/20.04/index.js
@@ -1,6 +1,7 @@
 const systemConfig = require('./系统配置');
+const installApp = require('./软件安装');
 
 module.exports = {
   title: '20.04',
-  children: [systemConfig]
+  children: [systemConfig, installApp]
 };
---
+++ docs/Linux/Ubuntu/20.04/软件安装/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: '软件安装',
+  children: [
+    {
+      title: '安装深度截屏',
+      path: '/Linux/Ubuntu/20.04/软件安装/安装深度截屏'
+    },
+    {
+      title: '安装flameshot(火焰截屏)',
+      path: '/Linux/Ubuntu/20.04/软件安装/安装flameshot'
+    },
+    {
+      title: '安装微信等常用Windows软件',
+      path: '/Linux/Ubuntu/20.04/软件安装/安装微信'
+    },
+  ]
+};
