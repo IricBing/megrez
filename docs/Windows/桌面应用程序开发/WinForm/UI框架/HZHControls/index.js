c3979bef1be4b5e2c984f85321a284405197e19f
IricBing95 <iricbing@gmail.com>
2021-09-10 17:52:18 +0800

    增加HZHControls安装笔记

--- docs/Windows/桌面应用程序开发/WinForm/UI框架/HZHControls/index.js
+++ docs/Windows/桌面应用程序开发/WinForm/UI框架/HZHControls/index.js
@@ -1,4 +1,7 @@
+const experience = require('./实践篇');
+
 module.exports = {
   title: 'HZHControls',
-  path: '/Windows/桌面应用程序开发/WinForm/UI框架/HZHControls/'
+  path: '/Windows/桌面应用程序开发/WinForm/UI框架/HZHControls/',
+  children: [experience]
 };
---
+++ docs/Windows/桌面应用程序开发/WinForm/UI框架/HZHControls/实践篇/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '实践篇',
+  children: [
+    {
+      title: '安装',
+      path: '/Windows/桌面应用程序开发/WinForm/UI框架/HZHControls/实践篇/安装'
+    }
+  ]
+};
