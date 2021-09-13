8f813b429aead9b81a323855f7c2f26d2a370d6f
Iric <iricbing@gmail.com>
2021-09-13 09:02:04 +0800

    增加

--- docs/Linux/index.js
+++ docs/Linux/index.js
@@ -1,6 +1,7 @@
-const ubuntu = require('./Ubuntu')
+const ubuntu = require('./Ubuntu');
+const base = require('./基础篇');
 
 module.exports = {
   title: 'Linux',
-  children: [ubuntu]
+  children: [base, ubuntu]
 };
---
+++ docs/Linux/基础篇/index.js
@@ -0,0 +1,6 @@
+const architecture = require('./底层架构');
+
+module.exports = {
+  title: '基础篇',
+  children: [architecture]
+};
