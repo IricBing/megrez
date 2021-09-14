80df9178105bbfbeefc2dde116bd878a68b902e8
Iric <iricbing@gmail.com>
2021-09-14 11:15:13 +0800

    增加Ubuntu安装DockerCompose笔记

--- docs/容器/Docker/index.js
+++ docs/容器/Docker/index.js
@@ -2,8 +2,9 @@ const baseUse = require('./基本使用');
 const baseKnowledge = require('./基础知识');
 const install = require('./安装');
 const imageBuild = require('./镜像构建');
+const dockerCompose = require('./DockerCompose');
 
 module.exports = {
   title: 'Docker',
-  children: [install, baseKnowledge, baseUse, imageBuild]
+  children: [install, baseKnowledge, baseUse, imageBuild, dockerCompose]
 };
