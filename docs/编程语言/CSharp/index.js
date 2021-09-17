f5319d0d9e2ecae27f3804984b4f962db918c399
IricBing95 <iricbing@gmail.com>
2021-09-17 14:12:44 +0800

    增加C#字符串和字节数组转换笔记

--- docs/编程语言/CSharp/index.js
+++ docs/编程语言/CSharp/index.js
@@ -1,7 +1,8 @@
 const base = require('./基础篇');
+const experience = require('./实践篇');
 
 module.exports = {
   title: 'C#',
   path: '/编程语言/CSharp/',
-  children: [base]
+  children: [base, experience]
 };
---
+++ docs/编程语言/CSharp/实践篇/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '实践篇',
+  children: [
+    {
+      title: '字符串与字节数组转换',
+      path: '/编程语言/CSharp/实践篇/字符串与字节数组转换'
+    }
+  ]
+};
