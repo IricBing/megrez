d2c762f80e0970c3edf9af2be8aac39c6e27de24
IricBing95 <iricbing@gmail.com>
2021-09-17 13:51:08 +0800

    增加C#枚举类型笔记

--- docs/编程语言/CSharp/基础篇/index.js
+++ docs/编程语言/CSharp/基础篇/index.js
@@ -1,5 +1,7 @@
 const start = require('./入门知识');
+const array = require('./数组');
+
 module.exports = {
   title: '基础篇',
-  children: [start]
+  children: [start, array]
 };
---
+++ docs/编程语言/CSharp/基础篇/数组/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: '数组',
+  path: '/编程语言/CSharp/基础篇/数组/',
+  children: [
+    {
+      title: 'enum: 枚举类型',
+      path: '/编程语言/CSharp/基础篇/数组/枚举类型'
+    }
+  ]
+};
