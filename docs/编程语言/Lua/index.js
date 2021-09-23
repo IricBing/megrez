78df5bcba41058f5620f25adf0acff99332e0ab3
Iric <iricbing@gmail.com>
2021-09-23 20:45:52 +0800

    增加Lua笔记

---
+++ docs/编程语言/Lua/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: 'Lua',
+  path: '/编程语言/Lua/'
+};
--- docs/编程语言/index.js
+++ docs/编程语言/index.js
@@ -2,8 +2,9 @@ const c = require('./C');
 const javascript = require('./JavaScript');
 const typescript = require('./TypeScript');
 const csharp = require('./CSharp');
+const lua = require('./Lua');
 
 module.exports = {
   title: '编程语言',
-  children: [c, javascript, typescript, csharp]
+  children: [c, javascript, typescript, csharp, lua]
 };
