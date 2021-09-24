05b9edd780dd2d3c4d2de308d7a8fcaab674e782
Iric <iricbing@gmail.com>
2021-09-24 18:46:52 +0800

    增加C#中类和对象部分笔记

--- docs/编程语言/CSharp/基础篇/index.js
+++ docs/编程语言/CSharp/基础篇/index.js
@@ -2,8 +2,9 @@ const start = require('./入门知识');
 const array = require('./数组');
 const baseLanguage = require('./基础语法');
 const delegateAndEvent = require('./委托和事件');
+const objectAndClass = require('./类和对象');
 
 module.exports = {
   title: '基础篇',
-  children: [start, baseLanguage, array, delegateAndEvent]
+  children: [start, baseLanguage, objectAndClass, array, delegateAndEvent]
 };
---
+++ docs/编程语言/CSharp/基础篇/类和对象/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: '类和对象',
+  children: [
+    {
+      title: '面向对象',
+      path: '/编程语言/CSharp/基础篇/类和对象/面向对象'
+    },
+    {
+      title: '类的定义',
+      path: '/编程语言/CSharp/基础篇/类和对象/类的定义'
+    },
+    {
+      title: '访问修饰符和修饰符',
+      path: '/编程语言/CSharp/基础篇/类和对象/访问修饰符和修饰符'
+    },
+    {
+      title: '方法的定义',
+      path: '/编程语言/CSharp/基础篇/类和对象/方法的定义'
+    }
+  ]
+};
