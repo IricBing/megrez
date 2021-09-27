f726277b6907b65c5dd8014902c8e73d42bd0a77
Iric <iricbing@gmail.com>
2021-09-27 23:05:35 +0800

    增加ab和nmap笔记

--- docs/运维/index.js
+++ docs/运维/index.js
@@ -3,8 +3,10 @@ const nginx = require('./Nginx');
 const sentry = require('./Sentry');
 const vercel = require('./Vercel');
 const frp = require('./frp');
+const ab = require('./ApacheBench');
+const nmap = require('./Nmap');
 
 module.exports = {
   title: '运维',
-  children: [letsencrypt, nginx, frp, sentry, vercel]
+  children: [letsencrypt, nginx, frp, sentry, vercel, ab, nmap]
 };
