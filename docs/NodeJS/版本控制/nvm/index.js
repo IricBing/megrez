59ae6d60e85dc8cfb6b709c46ce8413dab0a76f3
IricBing95 <iricbing@gmail.com>
2021-10-12 12:13:56 +0800

    增加ubuntu 安装nvm笔记

--- docs/NodeJS/版本控制/nvm/index.js
+++ docs/NodeJS/版本控制/nvm/index.js
@@ -3,6 +3,10 @@ module.exports = {
   path: '/NodeJS/版本控制/nvm/',
   children: [
     {
+      title: 'Ubuntu20.04 安装',
+      path: '/NodeJS/版本控制/nvm/Ubuntu20.04安装'
+    },
+    {
       title: '基础用法',
       path: '/NodeJS/版本控制/nvm/基础用法'
     }
