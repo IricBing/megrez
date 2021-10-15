33033e3b45d233109db4baaeab1d5739aa52d3e7
IricBing95 <iricbing@gmail.com>
2021-10-15 09:18:31 +0800

    增加cmder个性化配置笔记

--- docs/Windows/基础篇/终端/Cmder/index.js
+++ docs/Windows/基础篇/终端/Cmder/index.js
@@ -5,6 +5,10 @@ module.exports = {
     {
       title: '安装',
       path: '/Windows/基础篇/终端/Cmder/安装'
+    },
+    {
+      title: '个性化配置',
+      path: '/Windows/基础篇/终端/Cmder/个性化配置'
     }
   ]
 };
