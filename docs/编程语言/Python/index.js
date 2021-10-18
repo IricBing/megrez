ffd3a69b3bd00498df74b93ad9ad2ab6337aed9a
Iric <iricbing@gmail.com>
2021-10-18 14:08:14 +0800

    增加python print 笔记

--- docs/编程语言/Python/index.js
+++ docs/编程语言/Python/index.js
@@ -1,9 +1,10 @@
 const environment = require('./环境配置');
 const pip = require('./pip');
 const lib = require('./常用库');
+const practice = require('./实践积累');
 
 module.exports = {
   title: 'Python',
   path: '/编程语言/Python/',
-  children: [environment, pip, lib]
+  children: [environment, pip, lib, practice]
 };
---
+++ docs/编程语言/Python/实践积累/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '实践积累',
+  children: [
+    {
+      title: 'print 方法输出',
+      path: '/编程语言/Python/实践积累/print输出/'
+    }
+  ]
+};
