cb552f6c014b39d00b66bf58bc77202d122402ba
Iric <iricbing@gmail.com>
2021-10-19 12:24:34 +0800

    add PyCharm note

---
+++ docs/IDE/PyCharm/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'PyCharm',
+  children: [
+    {
+      title: '快捷键',
+      path: '/IDE/PyCharm/快捷键'
+    },
+    {
+      title: '配置conda环境',
+      path: '/IDE/PyCharm/配置conda环境/'
+    }
+  ]
+};
--- docs/IDE/index.js
+++ docs/IDE/index.js
@@ -1,8 +1,9 @@
 const vs = require('./VisualStudio');
 const androidStudio = require('./AndroidStudio');
 const vscode = require('./VSCode');
+const pycharm = require('./PyCharm');
 
 module.exports = {
   title: 'IDE',
-  children: [vscode, vs, androidStudio]
+  children: [vscode, vs, pycharm, androidStudio]
 };
