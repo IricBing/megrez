17b6f92a531d2a771508b10bf6f1cd317832a1b4
Iric <iricbing@gmail.com>
2021-10-20 19:00:07 +0800

    增加图片直方图笔记

--- docs/音视频/图片基础/index.js
+++ docs/音视频/图片基础/index.js
@@ -1,6 +1,7 @@
 const colorModel = require('./颜色模型');
+const histogram = require('./直方图');
 
 module.exports = {
   title: '图片基础',
-  children: [colorModel]
+  children: [colorModel, histogram]
 };
--- docs/音视频/图片基础/颜色模型/index.js
+++ docs/音视频/图片基础/颜色模型/index.js
@@ -3,8 +3,16 @@ module.exports = {
   path: '/音视频/图片基础/颜色模型/',
   children: [
     {
+      title: 'RGB',
+      path: '/音视频/图片基础/颜色模型/RGB/'
+    },
+    {
       title: 'HSV',
       path: '/音视频/图片基础/颜色模型/HSV/'
+    },
+    {
+      title: 'HSI',
+      path: '/音视频/图片基础/颜色模型/HSI/'
     },
     {
       title: 'HSL',
