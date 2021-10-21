6eae96701a247e7eff338c0b3e1561a31a384a44
IricBing95 <iricbing@gmail.com>
2021-10-21 16:05:55 +0800

    增加premiere 视频渲染笔记

--- docs/Adobe/Premiere/index.js
+++ docs/Adobe/Premiere/index.js
@@ -1,6 +1,7 @@
 const base = require('./基础篇');
+const render = require('./渲染篇');
 
 module.exports = {
   title: 'Premiere',
-  children: [base]
+  children: [base, render]
 };
---
+++ docs/Adobe/Premiere/渲染篇/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '渲染篇',
+  children: [
+    {
+      title: 'B站1080P视频导出',
+      path: '/Adobe/Premiere/渲染篇/B站1080P视频导出/'
+    }
+  ]
+};
