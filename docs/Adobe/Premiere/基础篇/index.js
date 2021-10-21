218939daf916292a32c2f71d18f7586f2de50e60
IricBing95 <iricbing@gmail.com>
2021-10-21 16:02:14 +0800

    增加premiere视频音量增大笔记

--- docs/Adobe/Premiere/基础篇/index.js
+++ docs/Adobe/Premiere/基础篇/index.js
@@ -3,7 +3,11 @@ module.exports = {
   children: [
     {
       title: '声音降噪',
-      path: '/Adobe/Premiere/基础篇/声音降噪'
+      path: '/Adobe/Premiere/基础篇/声音降噪/'
+    },
+    {
+      title: '视频音量增大',
+      path: '/Adobe/Premiere/基础篇/视频音量增大/'
     }
   ]
 };
