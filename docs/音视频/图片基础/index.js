a42d3cea25e4df6c9b5f4e4fa9c2b9c8e98d6fdf
Iric <iricbing@gmail.com>
2021-10-21 12:13:49 +0800

    add image binaryzation note

--- docs/音视频/图片基础/index.js
+++ docs/音视频/图片基础/index.js
@@ -1,7 +1,8 @@
 const colorModel = require('./颜色模型');
 const histogram = require('./直方图');
+const binaryzation = require('./二值化');
 
 module.exports = {
   title: '图片基础',
-  children: [colorModel, histogram]
+  children: [colorModel, histogram, binaryzation]
 };
