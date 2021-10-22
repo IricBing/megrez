210a243a519c98fff37d315cf6e8fe50984c7c50
Iric <iricbing@gmail.com>
2021-10-22 10:16:31 +0800

    add virtualbox network ntoe

--- docs/运维/VirtualBox/index.js
+++ docs/运维/VirtualBox/index.js
@@ -1,6 +1,7 @@
 const manager = require('./虚拟机管理');
+const network = require('./网络篇');
 
 module.exports = {
   title: 'VirtualBox',
-  children:[manager]
-}
+  children: [manager, network]
+};
---
+++ docs/运维/VirtualBox/网络篇/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: '网络篇',
+  path: '/运维/VirtualBox/网络篇/',
+  children: [
+    {
+      title: '网络地址转换（NAT）',
+      path: '/运维/VirtualBox/网络篇/网络地址转换'
+    },
+    {
+      title: '桥接网卡',
+      path: '/运维/VirtualBox/网络篇/桥接网卡'
+    },
+    {
+      title: '内部网络',
+      path: '/运维/VirtualBox/网络篇/内部网络'
+    },
+    {
+      title: 'host only 模式',
+      path: '/运维/VirtualBox/网络篇/host_only模式'
+    },
+    {
+      title: 'NAT 网络',
+      path: '/运维/VirtualBox/网络篇/NAT网络'
+    }
+  ]
+};
