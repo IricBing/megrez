49afb7539e27002559651fef31f58665da78c6b8
Iric <iricbing@gmail.com>
2021-10-22 09:51:06 +0800

    add alpine note

--- docs/Linux/index.js
+++ docs/Linux/index.js
@@ -1,8 +1,9 @@
 const ubuntu = require('./Ubuntu');
 const base = require('./基础篇');
 const sosumi = require('./sosumi');
+const alpine = require('./Alpine');
 
 module.exports = {
   title: 'Linux',
-  children: [base, ubuntu, sosumi]
+  children: [base, ubuntu, alpine, sosumi]
 };
