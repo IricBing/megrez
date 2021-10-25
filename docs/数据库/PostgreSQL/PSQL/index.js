680d01c2033081059955b46d0047cdec30d2e058
Iric <iricbing@gmail.com>
2021-10-25 15:21:48 +0800

    增PostgreSQL psql note

---
+++ docs/数据库/PostgreSQL/PSQL/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'PSQL',
+  path: '/数据库/PostgreSQL/PSQL/',
+  children: [
+    {
+      title: '数据库相关命令',
+      path: '/数据库/PostgreSQL/PSQL/数据库相关命令'
+    },
+    {
+      title: '表相关命令',
+      path: '/数据库/PostgreSQL/PSQL/表相关命令'
+    }
+  ]
+};
--- docs/数据库/PostgreSQL/index.js
+++ docs/数据库/PostgreSQL/index.js
@@ -1,7 +1,8 @@
 const newVersion = require('./版本新特性');
 const base = require('./基础篇');
+const psql = require('./PSQL');
 
 module.exports = {
   title: 'PostgreSQL',
-  children: [base, newVersion]
+  children: [base, psql, newVersion]
 };
