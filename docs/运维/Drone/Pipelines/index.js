ee33cd0392bda809b811190b05d136f7ceff1fc9
Iric <iricbing@gmail.com>
2021-10-28 10:51:17 +0800

    增加GitHub私有仓库集成

--- docs/运维/Drone/Pipelines/index.js
+++ docs/运维/Drone/Pipelines/index.js
@@ -5,5 +5,13 @@ const sshPipeline = require('./SSHPipeline');
 module.exports = {
   title: 'Pipelines',
   path: '/运维/Drone/Pipelines/',
-  children: [dockerPipeline, execPipeline, sshPipeline]
+  children: [
+    dockerPipeline,
+    execPipeline,
+    sshPipeline,
+    {
+      title: '接入GitHub私有仓库',
+      path: '/运维/Drone/Pipelines/GitHub私有仓库'
+    }
+  ]
 };
