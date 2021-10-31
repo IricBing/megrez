58b34c8ca7705ac9c0e27f4aada43ae749274b90
Iric <iricbing@gmail.com>
2021-10-31 22:41:19 +0800

    完成python number类型

--- docs/编程语言/Python/语言基础/index.js
+++ docs/编程语言/Python/语言基础/index.js
@@ -1,4 +1,5 @@
 const taxeme = require('./语法特点');
+const dataType = require('./基本数据类型');
 
 module.exports = {
   title: '语言基础',
@@ -7,6 +8,7 @@ module.exports = {
     {
       title: '变量',
       path: '/编程语言/Python/语言基础/变量'
-    }
+    },
+    dataType
   ]
 };
---
+++ docs/编程语言/Python/语言基础/基本数据类型/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: '基本数据类型',
+  children: [
+    {
+      title: 'Number（数字类型）',
+      path: '/编程语言/Python/语言基础/基本数据类型/Number'
+    },
+    {
+      title: 'String（字符串类型）',
+      path: '/编程语言/Python/语言基础/基本数据类型/String'
+    },
+    {
+      title: 'Boolean（布尔类型）',
+      path: '/编程语言/Python/语言基础/基本数据类型/Boolean'
+    },
+    {
+      title: '类型转换',
+      path: '/编程语言/Python/语言基础/基本数据类型/类型转换'
+    }
+  ]
+};
