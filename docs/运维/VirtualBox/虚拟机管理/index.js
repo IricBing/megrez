61e14bb3732ff7e2fd9a144bc5fe1559b40ebed7
Iric <iricbing@gmail.com>
2021-11-09 09:54:42 +0800

    增加VirtualBox控制台别名笔记

--- docs/运维/VirtualBox/虚拟机管理/index.js
+++ docs/运维/VirtualBox/虚拟机管理/index.js
@@ -4,6 +4,10 @@ module.exports = {
     {
       title: '控制台管理',
       path: '/运维/VirtualBox/虚拟机管理/控制台管理'
+    },
+    {
+      title: '命令行别名设置',
+      path: '/运维/VirtualBox/虚拟机管理/命令行别名设置'
     }
   ]
 };
