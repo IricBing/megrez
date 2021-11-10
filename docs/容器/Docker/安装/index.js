e2da01c1e923fff4e4fddc13a0493f824afa62c3
Iric <iricbing@gmail.com>
2021-11-10 12:30:54 +0800

    增加docker走代理笔记

--- docs/容器/Docker/安装/index.js
+++ docs/容器/Docker/安装/index.js
@@ -4,6 +4,10 @@ module.exports = {
     {
       title: 'Ubuntu20.04安装',
       path: '/容器/Docker/安装/Ubuntu20.04安装'
+    },
+    {
+      title: '代理配置',
+      path: '/容器/Docker/安装/代理配置'
     }
   ]
 }
