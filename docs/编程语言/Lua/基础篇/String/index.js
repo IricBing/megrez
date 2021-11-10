9f5b31c70536fe43a1e2ed4550879029b0202083
Iric <iricbing@gmail.com>
2021-11-10 22:43:14 +0800

    增加lua字符串笔记

---
+++ docs/编程语言/Lua/基础篇/String/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: 'String（字符串）',
+  path: '/编程语言/Lua/基础篇/String/',
+  children: [
+    {
+      title: '字符串常量',
+      path: '/编程语言/Lua/基础篇/String/字符串常量'
+    },
+    {
+      title: '长字符串/多行字符串',
+      path: '/编程语言/Lua/基础篇/String/长字符串'
+    },
+    {
+      title: '强制类型转换',
+      path: '/编程语言/Lua/基础篇/String/强制类型转换'
+    },
+    {
+      title: '字符串标准库',
+      path: '/编程语言/Lua/基础篇/String/字符串标准库'
+    },
+    {
+      title: 'Unicode编码',
+      path: '/编程语言/Lua/基础篇/String/Unicode编码'
+    }
+  ]
+};
