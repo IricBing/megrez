a40f9c1c64af329f8515e8f096c72081d9bddc98
Iric <iricbing@gmail.com>
2021-11-12 12:16:04 +0800

    增加Lua命名规范笔记

--- docs/容器/Kubernetes/命令篇/kubeadm/init.md
+++ docs/容器/Kubernetes/命令篇/kubeadm/init.md
@@ -1,1 +1,9 @@
 # kubeadm init
+
+此命令初始化一个 `Kubernetes` 控制平面节点。
+
+## 语法
+
+```shell
+$ kubeadm init [flags]
+```
--- docs/编程语言/Lua/index.js
+++ docs/编程语言/Lua/index.js
@@ -1,7 +1,8 @@
 const base = require('./基础篇');
+const practice = require('./实践篇');
 
 module.exports = {
   title: 'Lua',
   path: '/编程语言/Lua/',
-  children: [base]
+  children: [base, practice]
 };
