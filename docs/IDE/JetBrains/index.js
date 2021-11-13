dee9ac5474e59e476251bc17b5732d0122fbbc59
Iric <iricbing@gmail.com>
2021-11-13 16:33:33 +0800

    增加Jetbrains2021全系破解笔记

---
+++ docs/IDE/JetBrains/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'JetBrains',
+  children: [
+    {
+      title: '2021全系破解',
+      path: '/IDE/JetBrains/2021全系破解'
+    }
+  ]
+}
--- docs/IDE/index.js
+++ docs/IDE/index.js
@@ -2,8 +2,9 @@ const vs = require('./VisualStudio');
 const androidStudio = require('./AndroidStudio');
 const vscode = require('./VSCode');
 const pycharm = require('./PyCharm');
+const jetbrains = require('./JetBrains');
 
 module.exports = {
   title: 'IDE',
-  children: [vscode, vs, pycharm, androidStudio]
+  children: [vscode, vs, pycharm, androidStudio, jetbrains]
 };
