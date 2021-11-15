c764000b34c821fd13de3269efe6cbba09d96be7
Iric <iricbing@gmail.com>
2021-11-15 11:28:34 +0800

    增加ip地理信息笔记

--- docs/实践积累/index.js
+++ docs/实践积累/index.js
@@ -1,6 +1,7 @@
 const csv = require('./csv文件');
+const ipRegion = require('./ip地理信息');
 
 module.exports = {
   title: '实践积累',
-  children: [csv]
+  children: [csv, ipRegion]
 };
---
+++ docs/实践积累/ip地理信息/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: 'ip地理信息',
+  path: '/实践积累/ip地理信息/'
+};
