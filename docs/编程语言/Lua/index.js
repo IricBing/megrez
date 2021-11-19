3e3aba80558d8d774a423a974dff4b13e5bac779
Iric <iricbing@gmail.com>
2021-11-19 16:46:35 +0800

    增加Lua设计模式

--- docs/编程语言/Lua/index.js
+++ docs/编程语言/Lua/index.js
@@ -1,8 +1,9 @@
 const base = require('./基础篇');
 const practice = require('./实践篇');
+const designPatterns = require('./设计模式');
 
 module.exports = {
   title: 'Lua',
   path: '/编程语言/Lua/',
-  children: [base, practice]
+  children: [base, practice, designPatterns]
 };
---
+++ docs/编程语言/Lua/设计模式/index.js
@@ -0,0 +1,7 @@
+const create = require('./创建型模式');
+
+module.exports = {
+  title: '设计模式',
+  path: '/编程语言/Lua/设计模式/',
+  children: [create]
+};
