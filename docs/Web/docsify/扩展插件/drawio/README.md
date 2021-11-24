398fe562ab059e6fe07a4281aee6dfc9856f2d8d
Iric <iricbing@gmail.com>
2021-11-24 23:31:31 +0800

    update docsify note

---
+++ docs/Web/docsify/扩展插件/drawio/README.md
@@ -0,0 +1,3 @@
+# drawio支持
+
+[GitHub地址](https://github.com/KonghaYao/docsify-drawio)
--- docs/Web/docsify/扩展插件/index.js
+++ docs/Web/docsify/扩展插件/index.js
@@ -2,8 +2,12 @@ module.exports = {
   title: '扩展插件',
   children: [
     {
-      title: '右侧锚点导航',
-      path: '/Web/docsify/扩展插件/右侧锚点导航/'
+      title: 'toc(右侧锚点导航)',
+      path: '/Web/docsify/扩展插件/toc/'
+    },
+    {
+      title: 'drawio支持',
+      path: '/Web/docsify/扩展插件/drawio/'
     }
   ]
 };
