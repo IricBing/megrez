d0297b33ba73d825303b96d9b63f177b7bbcb940
Iric <iricbing@gmail.com>
2021-11-25 08:30:24 +0800

    add nginx user note

--- docs/运维/Nginx/index.js
+++ docs/运维/Nginx/index.js
@@ -1,5 +1,6 @@
+const base = require('./基础篇');
 const config = require('./配置篇');
-const errorHandler = require('./错误处理')
+const errorHandler = require('./错误处理');
 
 module.exports = {
   title: 'Nginx',
@@ -8,6 +9,7 @@ module.exports = {
       title: '版本说明',
       path: '/运维/Nginx/版本说明'
     },
+    base,
     config,
     errorHandler
   ]
---
+++ docs/运维/Nginx/基础篇/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '基础篇',
+  children: [
+    {
+      title: '用户',
+      path: '/运维/Nginx/基础篇/用户'
+    }
+  ]
+};
