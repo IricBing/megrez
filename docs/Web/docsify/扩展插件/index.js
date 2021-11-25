368563c2ed0bbb620c3ec711212a5d9e77730b4b
Iric <iricbing@gmail.com>
2021-11-25 11:32:59 +0800

    update docsify 高亮块笔记

--- docs/Web/docsify/扩展插件/index.js
+++ docs/Web/docsify/扩展插件/index.js
@@ -1,9 +1,14 @@
+
 module.exports = {
   title: '扩展插件',
   children: [
     {
       title: 'toc(右侧锚点导航)',
       path: '/Web/docsify/扩展插件/toc/'
+    },
+    {
+      title: '高亮块',
+      path: '/Web/docsify/扩展插件/高亮块/'
     },
     {
       title: 'drawio支持',
---
+++ docs/Web/docsify/扩展插件/高亮块/README.md
@@ -0,0 +1,3 @@
+# 高亮块
+
+采用插件[docsify-plugin-flexible-alerts](https://github.com/fzankl/docsify-plugin-flexible-alerts)实现。
