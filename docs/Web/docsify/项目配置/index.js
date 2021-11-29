782518abb1431d9e3de7e40dac85e0fe06d01e52
Iric <iricbing@gmail.com>
2021-11-29 19:04:42 +0800

    增加Docsify sidebar配置笔记

--- docs/Web/docsify/项目配置/index.js
+++ docs/Web/docsify/项目配置/index.js
@@ -2,6 +2,10 @@ module.exports = {
   title: '项目配置',
   children: [
     {
+      title: '侧边栏',
+      path: '/Web/docsify/项目配置/侧边栏/'
+    },
+    {
       title: 'mermaid',
       path: '/Web/docsify/项目配置/mermaid/'
     },
