6b0877c64c9e02ba3ea736eab7c2ab42ae0a4b80
Iric <iricbing@gmail.com>
2021-12-09 11:20:00 +0800

    增加node代码混淆笔记

---
+++ docs/NodeJS/实践积累/代码混淆/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: '代码混淆',
+  path: '/NodeJS/实践积累/代码混淆/',
+  children: [
+    {
+      title: 'terser',
+      path: '/NodeJS/实践积累/代码混淆/terser'
+    }
+  ]
+};
---
+++ docs/NodeJS/实践积累/代码混淆/terser.md
@@ -0,0 +1,5 @@
+# terser
+
+[github地址](https://github.com/terser/terser)
+
+[官网](https://terser.org/)
