91adbdd47d6a171532e2c3c18098ff7202f73f2e
Iric <iricbing@gmail.com>
2021-12-30 18:02:52 +0800

    增加Electron不支持pnpm笔记

--- docs/Web/Electron/踩坑集锦/index.js
+++ docs/Web/Electron/踩坑集锦/index.js
@@ -1,6 +1,7 @@
 const start = require('./启动相关');
+const nosupportPnpm = require('./不支持pnpm');
 
 module.exports = {
   title: '踩坑集锦',
-  children: [start]
+  children: [start, nosupportPnpm]
 };
---
+++ docs/Web/Electron/踩坑集锦/不支持pnpm/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: '不支持pnpm',
+  path: '/Web/Electron/踩坑集锦/不支持pnpm/'
+};
