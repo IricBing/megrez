5662a19e0b143cdc1c956b331ec1a867ca4c14f2
Iric <iricbing@gmail.com>
2022-01-20 16:39:23 +0800

    增加ServiceWorker笔记目录

---
+++ docs/Web/基础篇/PWA/ServiceWorker/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: 'ServiceWorker',
+  path: '/Web/基础篇/PWA/ServiceWorker/'
+};
--- docs/Web/基础篇/PWA/index.js
+++ docs/Web/基础篇/PWA/index.js
@@ -1,7 +1,8 @@
 const workbox = require('./workbox');
+const serviceWorker = require('./ServiceWorker');
 
 module.exports = {
   title: 'PWA',
   path: '/Web/基础篇/PWA/',
-  children: [workbox]
+  children: [serviceWorker, workbox]
 };
