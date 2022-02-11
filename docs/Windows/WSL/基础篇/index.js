5d56e5cfd6bfe5d56ea269f75d229bfd3decc8e6
Iric <iricbing@gmail.com>
2022-02-11 16:53:46 +0800

    增加WSL文件资源管理器部分笔记

--- docs/Windows/WSL/基础篇/index.js
+++ docs/Windows/WSL/基础篇/index.js
@@ -1,7 +1,8 @@
 const baseCMD = require('./基本命令');
 const vs = require('./WSL1vsWSL2');
+const explorer = require('./文件管理器');
 
 module.exports = {
   title: '基础篇',
-  children: [baseCMD, vs]
+  children: [baseCMD, explorer, vs]
 };
---
+++ docs/Windows/WSL/基础篇/文件管理器/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: '文件管理器',
+  path: '/Windows/WSL/基础篇/文件管理器/'
+};
