d0fff613704224dad5c82187690892ffd1276be5
Iric <iricbing@gmail.com>
2022-02-23 10:30:14 +0800

    增加微信公众平台模板消息部分笔记

--- docs/Tencent/微信公众平台/服务号/功能点/index.js
+++ docs/Tencent/微信公众平台/服务号/功能点/index.js
@@ -1,6 +1,7 @@
 const recommendedAttentionAfterPayment = require('./支付后推荐关注');
+const templateMessage = require('./模板消息');
 
 module.exports = {
   title: '功能点',
-  children: [recommendedAttentionAfterPayment]
+  children: [recommendedAttentionAfterPayment, templateMessage]
 };
---
+++ docs/Tencent/微信公众平台/服务号/功能点/模板消息/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: '模板消息',
+  path: '/Tencent/微信公众平台/服务号/功能点/模板消息/'
+};
