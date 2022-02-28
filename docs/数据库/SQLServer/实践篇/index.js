249eaa0aa2876696a1592934e488dd18b54f7470
Iric <iricbing@gmail.com>
2022-02-28 18:14:00 +0800

    增加SQL Server忘记sa密码笔记

--- docs/数据库/SQLServer/实践篇/index.js
+++ docs/数据库/SQLServer/实践篇/index.js
@@ -1,6 +1,7 @@
 const auth = require('./身份认证');
+const forgetPassword = require('./忘记sa用户密码');
 
 module.exports = {
   title: '实践篇',
-  children:[auth]
+  children: [auth, forgetPassword]
 };
---
+++ docs/数据库/SQLServer/实践篇/忘记sa用户密码/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: '忘记sa用户密码',
+  path: '/数据库/SQLServer/实践篇/忘记sa用户密码/'
+};
