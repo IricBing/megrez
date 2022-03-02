8795fd996c87572cef13853ef016f36a193cfea8
Iric <iricbing@gmail.com>
2022-03-02 14:27:58 +0800

    增加批处理注释部分笔记

--- docs/编程语言/bat/批处理基础/index.js
+++ docs/编程语言/bat/批处理基础/index.js
@@ -1,6 +1,7 @@
 const command = require('./命令汇总');
+const attentions = require('./注释');
 
 module.exports = {
   title: '批处理基础',
-  children: [command]
+  children: [command, attentions]
 };
---
+++ docs/编程语言/bat/批处理基础/注释/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: '注释',
+  path: '/编程语言/bat/批处理基础/注释/'
+};
