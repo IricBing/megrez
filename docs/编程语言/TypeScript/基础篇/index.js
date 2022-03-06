3d0460012ecd602392067627581ad9d9e29158c6
Iric <iricbing@gmail.com>
2022-03-06 13:47:44 +0800

    增加部分TS装饰器部分笔记

--- docs/编程语言/TypeScript/基础篇/index.js
+++ docs/编程语言/TypeScript/基础篇/index.js
@@ -1,6 +1,7 @@
 const type = require('./类型');
+const decorator = require('./装饰器');
 
 module.exports = {
   title: '基础篇',
-  children: [type]
+  children: [type, decorator]
 };
---
+++ docs/编程语言/TypeScript/基础篇/装饰器/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: '装饰器',
+  path: '/编程语言/TypeScript/基础篇/装饰器/',
+  children: [
+    {
+      title: '类装饰器',
+      path: '/编程语言/TypeScript/基础篇/装饰器/类装饰器'
+    },
+    {
+      title: '方法装饰器',
+      path: '/编程语言/TypeScript/基础篇/装饰器/方法装饰器'
+    },
+    {
+      title: '访问器装饰器',
+      path: '/编程语言/TypeScript/基础篇/装饰器/访问器装饰器'
+    },
+    {
+      title: '属性装饰器',
+      path: '/编程语言/TypeScript/基础篇/装饰器/属性装饰器'
+    },
+    {
+      title: '参数装饰器',
+      path: '/编程语言/TypeScript/基础篇/装饰器/参数装饰器'
+    },
+    {
+      title: '装饰器工厂与组合',
+      path: '/编程语言/TypeScript/基础篇/装饰器/装饰器工厂与组合'
+    }
+  ]
+};
