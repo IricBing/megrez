267199ff95245678b0dfb6001fbedb1f0084c006
Iric <iricbing@gmail.com>
2022-03-10 10:54:22 +0800

    增加OPC协议笔记

---
+++ docs/协议/应用层/OPC/UA/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: 'OPC UA',
+  path: '/协议/应用层/OPC/UA/'
+};
--- docs/协议/应用层/OPC/index.js
+++ docs/协议/应用层/OPC/index.js
@@ -1,4 +1,7 @@
+const ua = require('./UA');
+
 module.exports = {
   title: 'OPC',
   path: '/协议/应用层/OPC/',
-}
+  children: [ua]
+};
