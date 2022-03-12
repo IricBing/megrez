e1a733e3c159cc76d5b9d97ce84f5cfb05619fda
Iric <iricbing@gmail.com>
2022-03-12 18:21:21 +0800

    增加部分位运算笔记

--- docs/编程语言/程序基础/位运算/index.js
+++ docs/编程语言/程序基础/位运算/index.js
@@ -1,4 +1,8 @@
+const utils = require('./小技巧');
+const question = require('./经典问题');
+
 module.exports = {
   title: '位运算',
-  path: '/编程语言/程序基础/位运算/'
+  path: '/编程语言/程序基础/位运算/',
+  children: [utils, question]
 };
---
+++ docs/编程语言/程序基础/位运算/小技巧/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: '小技巧',
+  path: '/编程语言/程序基础/位运算/小技巧/'
+};
