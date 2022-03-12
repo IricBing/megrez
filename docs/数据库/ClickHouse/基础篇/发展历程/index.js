5e5d5286212e678f25941870adc651ad921d845e
Iric <iricbing@gmail.com>
2022-03-12 16:41:01 +0800

    增加ClickHouse metragel时期笔记

--- docs/数据库/ClickHouse/基础篇/发展历程/index.js
+++ docs/数据库/ClickHouse/基础篇/发展历程/index.js
@@ -4,19 +4,19 @@ module.exports = {
   children: [
     {
       title: '顺理成章的MySQL时期',
-      path: '/数据库/ClickHouse/基础篇/顺理成章的MySQL时期'
+      path: '/数据库/ClickHouse/基础篇/发展历程/顺理成章的MySQL时期'
     },
     {
       title: '另辟蹊径的Metrage时期',
-      path: '/数据库/ClickHouse/基础篇/另辟蹊径的Metrage时期'
+      path: '/数据库/ClickHouse/基础篇/发展历程/另辟蹊径的Metrage时期'
     },
     {
       title: '自我突破的OLAPServer时期',
-      path: '/数据库/ClickHouse/基础篇/自我突破的OLAPServer时期'
+      path: '/数据库/ClickHouse/基础篇/发展历程/自我突破的OLAPServer时期'
     },
     {
       title: '水到渠成的ClickHouse时代',
-      path: '/数据库/ClickHouse/基础篇/水到渠成的ClickHouse时代'
+      path: '/数据库/ClickHouse/基础篇/发展历程/水到渠成的ClickHouse时代'
     }
   ]
 };
