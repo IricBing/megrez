45cb756115c3e80aa44e4a96bb462ffa7a607e12
Iric <iricbing@gmail.com>
2022-03-14 18:47:09 +0800

    增加ClickHouse数据定义部分笔记

--- docs/数据库/ClickHouse/基础篇/index.js
+++ docs/数据库/ClickHouse/基础篇/index.js
@@ -1,6 +1,7 @@
 const roadmap = require('./发展历程');
+const defineData = require('./数据定义');
 
 module.exports = {
   title: '基础篇',
-  children: [roadmap]
+  children: [roadmap, defineData]
 };
---
+++ docs/数据库/ClickHouse/基础篇/数据定义/index.js
@@ -0,0 +1,7 @@
+const dataType = require('./数据类型');
+
+module.exports = {
+  title: '数据定义',
+  path: '/数据库/ClickHouse/基础篇/数据定义/',
+  children: [dataType]
+};
