d8e853b5481264b5695eb3c9dc1cd669f9bd104f
Iric <iricbing@gmail.com>
2022-03-16 21:31:00 +0800

    增加函数奇偶性笔记

--- docs/考研/数二/高中前置知识/认识函数/函数与反函数/特性/index.js
+++ docs/考研/数二/高中前置知识/认识函数/函数与反函数/特性/index.js
@@ -1,7 +1,8 @@
 const boundedness = require('./有界性');
 const monotonicity = require('./单调性');
+const parity = require('./奇偶性');
 
 module.exports = {
   title: '特性',
-  children: [boundedness, monotonicity]
+  children: [boundedness, monotonicity, parity]
 };
