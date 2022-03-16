7c2d15f97d99f72e50b70d72e5f8df6f696faa73
Iric <iricbing@gmail.com>
2022-03-16 20:42:51 +0800

    增加映射与反映社笔记

--- docs/考研/数二/高中前置知识/认识函数/函数与反函数/index.js
+++ docs/考研/数二/高中前置知识/认识函数/函数与反函数/index.js
@@ -1,4 +1,9 @@
+const mapping = require('./映射');
+const inverseMapping = require('./逆映射');
+const functionAndAntiFunction = require('./函数与反函数');
+
 module.exports = {
   title: '函数与反函数',
-  path: '/考研/数二/高中前置知识/认识函数/函数与反函数/'
+  path: '/考研/数二/高中前置知识/认识函数/函数与反函数/',
+  children: [mapping, inverseMapping, functionAndAntiFunction]
 };
