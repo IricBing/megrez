60d8b7c3109869ccbdf3c2abdb91362753e4cebe
Iric <iricbing@gmail.com>
2022-03-16 11:48:32 +0800

    增加vue原理篇笔记

--- docs/Web/Vue2/index.js
+++ docs/Web/Vue2/index.js
@@ -1,7 +1,8 @@
 const appConfig = require('./工程配置');
 const practice = require('./实践篇');
+const principle = require('./原理篇');
 
 module.exports = {
   title: 'Vue 2.x',
-  children: [appConfig, practice]
+  children: [appConfig, principle, practice]
 };
