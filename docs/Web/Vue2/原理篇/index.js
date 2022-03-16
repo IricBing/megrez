b0fec127da4a9f3f9f968c7a8ff68472c3003d01
Iric <iricbing@gmail.com>
2022-03-16 11:51:19 +0800

    fix bug

---
+++ docs/Web/Vue2/原理篇/index.js
@@ -0,0 +1,6 @@
+const bind = require('./双向绑定');
+
+module.exports = {
+  title: '原理篇',
+  children: [bind]
+};
---
+++ docs/Web/Vue2/原理篇/双向绑定/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: '双向绑定',
+  path: '/Web/Vue2/原理篇/双向绑定/'
+};
