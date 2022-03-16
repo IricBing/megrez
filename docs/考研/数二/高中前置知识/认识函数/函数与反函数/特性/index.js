38010dde0658837b82b31f7192a156ad016a0ab4
Iric <iricbing@gmail.com>
2022-03-16 21:37:03 +0800

    增加函数周期性笔记

--- docs/考研/数二/高中前置知识/认识函数/函数与反函数/特性/index.js
+++ docs/考研/数二/高中前置知识/认识函数/函数与反函数/特性/index.js
@@ -1,8 +1,9 @@
 const boundedness = require('./有界性');
 const monotonicity = require('./单调性');
 const parity = require('./奇偶性');
+const periodic = require('./周期性');
 
 module.exports = {
   title: '特性',
-  children: [boundedness, monotonicity, parity]
+  children: [boundedness, monotonicity, parity, periodic]
 };
