6a3718f96ab56efaa940e9dfefbc9d5586bfe1a7
Iric <iricbing@gmail.com>
2022-03-21 09:44:41 +0800

    增加ClickHouse类型目录

--- docs/数据库/ClickHouse/基础篇/数据定义/数据类型/基础类型/index.js
+++ docs/数据库/ClickHouse/基础篇/数据定义/数据类型/基础类型/index.js
@@ -1,4 +1,9 @@
+const numberType = require('./数值类型');
+const stringType = require('./字符串类型');
+const dateType = require('./时间类型');
+
 module.exports = {
   title: '基础类型',
-  path: '/数据库/ClickHouse/基础篇/数据定义/数据类型/基础类型/'
+  path: '/数据库/ClickHouse/基础篇/数据定义/数据类型/基础类型/',
+  children: [numberType, stringType, dateType]
 };
