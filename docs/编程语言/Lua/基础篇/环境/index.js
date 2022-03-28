8fc62779e354ad8c433d42c500624704a7af0310
Iric <iricbing@gmail.com>
2022-03-28 21:14:38 +0800

    增加Lua环境部分笔记

--- docs/编程语言/Lua/基础篇/环境/index.js
+++ docs/编程语言/Lua/基础篇/环境/index.js
@@ -1,4 +1,30 @@
 module.exports = {
   title: '环境',
   path: '/编程语言/Lua/基础篇/环境/',
-}
+  children: [
+    {
+      title: '具有动态名称的全局变量',
+      path: '/编程语言/Lua/基础篇/环境/具有动态名称的全局变量'
+    },
+    {
+      title: '全局变量的声明',
+      path: '/编程语言/Lua/基础篇/环境/全局变量的声明'
+    },
+    {
+      title: '非全局环境',
+      path: '/编程语言/Lua/基础篇/环境/非全局环境'
+    },
+    {
+      title: '使用 _ENV',
+      path: '/编程语言/Lua/基础篇/环境/使用_ENV'
+    },
+    {
+      title: '环境和模块',
+      path: '/编程语言/Lua/基础篇/环境/环境和模块'
+    },
+    {
+      title: '_ENV 和 load',
+      path: '/编程语言/Lua/基础篇/环境/_ENV和load'
+    }
+  ]
+};
