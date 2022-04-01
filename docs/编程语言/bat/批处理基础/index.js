2b43cd4acdba16570fd7b18f6b7a174be5fdde12
Iric <iricbing@gmail.com>
2022-04-01 22:15:55 +0800

    增加bat title 和 Color笔记

--- docs/编程语言/bat/批处理基础/index.js
+++ docs/编程语言/bat/批处理基础/index.js
@@ -3,8 +3,9 @@ const attentions = require('./注释');
 const echo = require('./echo');
 const pause = require('./PAUSE');
 const errorLevel = require('./errorlevel');
+const title = require('./title');
 
 module.exports = {
   title: '批处理基础',
-  children: [command, attentions, echo, pause, errorLevel]
+  children: [command, attentions, echo, pause, errorLevel, title]
 };
---
+++ docs/编程语言/bat/批处理基础/title/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: 'title',
+  path: '/编程语言/bat/批处理基础/title/'
+};
