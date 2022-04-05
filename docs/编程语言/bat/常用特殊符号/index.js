b825463a575e08f94b98c714a83354d09d73e3be
Iric <iricbing@gmail.com>
2022-04-05 17:05:45 +0800

    增加bat 变量引导符笔记

--- docs/编程语言/bat/常用特殊符号/index.js
+++ docs/编程语言/bat/常用特殊符号/index.js
@@ -1,7 +1,8 @@
 const at = require('./命令⾏回显屏蔽符');
+const guide = require('./批处理变量引导符');
 
 module.exports = {
   title: '常用特殊符号',
   path: '/编程语言/bat/常用特殊符号/',
-  children: [at]
+  children: [at, guide]
 };
