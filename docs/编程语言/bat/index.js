620c194fae7053ad2dc6c6fbb1d794c3d906d1d0
Iric <iricbing@gmail.com>
2022-04-05 17:00:54 +0800

    增加bat 常用特殊符号笔记

--- docs/编程语言/bat/index.js
+++ docs/编程语言/bat/index.js
@@ -1,7 +1,7 @@
 const resources = require('./参考资源');
 const basic = require('./批处理基础');
 const practice = require('./实践积累');
-const specialSymbol = require('./常⽤特殊符号');
+const specialSymbol = require('./常用特殊符号');
 
 module.exports = {
   title: 'bat',
