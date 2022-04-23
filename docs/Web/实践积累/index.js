af4cf792313218992beed6f2e03bcb72e9f519c3
Iric <iricbing@gmail.com>
2022-04-23 13:21:29 +0800

    增加web积累中的大文件上传部分笔记

--- docs/Web/实践积累/index.js
+++ docs/Web/实践积累/index.js
@@ -3,8 +3,9 @@ const mobileHttpSlow = require('./手机端接口反应慢');
 const chromeDebugMobileWeb = require('./chrome调试手机端网页');
 const tts = require('./语音播报');
 const serialAPI = require('./WebSerialAPI');
+const bigFileUpload = require('./大文件上传');
 
 module.exports = {
   title: '实践积累',
-  children: [browserDownloadFile, mobileHttpSlow, chromeDebugMobileWeb, tts, serialAPI]
+  children: [browserDownloadFile, mobileHttpSlow, chromeDebugMobileWeb, tts, serialAPI, bigFileUpload]
 };
---
+++ docs/Web/实践积累/大文件上传/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: "大文件上传",
+  path: '/Web/实践积累/大文件上传/'
+};
