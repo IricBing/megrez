11c7b01179a6064761f46ae5203e038792b1b48e
Iric <iricbing@gmail.com>
2022-04-24 14:53:21 +0800

    增加ClickHouse数据查询部分笔记

--- docs/数据库/ClickHouse/基础篇/index.js
+++ docs/数据库/ClickHouse/基础篇/index.js
@@ -2,8 +2,9 @@ const roadmap = require('./发展历程');
 const defineData = require('./数据定义');
 const arch = require('./架构概述');
 const dataDictionary = require('./数据字典');
+const dataQuery = require('./数据查询');
 
 module.exports = {
   title: '基础篇',
-  children: [roadmap, arch, defineData, dataDictionary]
+  children: [roadmap, arch, defineData, dataDictionary, dataQuery]
 };
---
+++ docs/数据库/ClickHouse/基础篇/数据查询/index.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  title: '数据查询',
+  path: '/数据库/ClickHouse/基础篇/数据查询/',
+}
