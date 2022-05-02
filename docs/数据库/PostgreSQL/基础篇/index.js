41b383a9491aaf35a06ff7ea798c5606f94d8f84
Iric <iricbing@gmail.com>
2022-05-02 16:22:25 +0800

    增加PostgreSQL插入数据笔记目录

--- docs/数据库/PostgreSQL/基础篇/index.js
+++ docs/数据库/PostgreSQL/基础篇/index.js
@@ -3,8 +3,9 @@ const baseOperate = require('./基本操作');
 const dataType = require('./数据类型');
 const operator = require('./运算符');
 const func = require('./函数');
+const insertUpdateDelete = require('./插入更新与删除数据');
 
 module.exports = {
   title: '基础篇',
-  children: [install, baseOperate, dataType, operator, func]
+  children: [install, baseOperate, dataType, operator, func, insertUpdateDelete]
 };
---
+++ docs/数据库/PostgreSQL/基础篇/插入更新与删除数据/index.js
@@ -0,0 +1,6 @@
+const insert = require('./插入数据');
+
+module.exports = {
+  title: '插入、更新与删除数据',
+  children: [insert]
+};
