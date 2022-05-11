a619b53e5c67aeec6377521be58258b546b4f426
Iric <iricbing@gmail.com>
2022-05-11 22:17:35 +0800

    fix 部分404文档

--- docs/数据库/MySQL/index.js
+++ docs/数据库/MySQL/index.js
@@ -1,6 +1,7 @@
 const env = require('./环境篇');
+const practice = require('./实践篇');
 
 module.exports = {
   title: 'MySQL',
-  children: [env]
+  children: [env, practice]
 };
--- docs/数据库/MySQL/环境篇/index.js
+++ docs/数据库/MySQL/环境篇/index.js
@@ -1,7 +1,6 @@
 const install = require('./安装');
-const practice = require('./实践篇');
 
 module.exports = {
   title: '环境篇',
-  children: [install, practice]
+  children: [install]
 };
