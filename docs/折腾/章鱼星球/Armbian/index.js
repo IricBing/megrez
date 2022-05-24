a37296f200df2ecbbb9e3d63534e512e45b0e681
Iric <iricbing@gmail.com>
2022-05-24 23:02:46 +0800

    增加章鱼星球Armbian配置

---
+++ docs/折腾/章鱼星球/Armbian/index.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  title: 'Armbian',
+  path: '/折腾/章鱼星球/Armbian/',
+}
--- docs/折腾/章鱼星球/index.js
+++ docs/折腾/章鱼星球/index.js
@@ -1,9 +1,10 @@
 const burnSuite = require('./刷机套装');
 const firmwareResource = require('./固件资源');
 const openwrt = require('./OpenWrt');
+const armbian = require('./Armbian');
 
 module.exports = {
   title: '章鱼星球',
   path: '/折腾/章鱼星球/',
-  children: [burnSuite, firmwareResource, openwrt]
+  children: [burnSuite, firmwareResource, openwrt, armbian]
 };
