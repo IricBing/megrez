ae73dc856875c04a51f7c96a1010bada67cd44e7
Iric <iricbing@gmail.com>
2022-05-25 17:04:20 +0800

    增加订阅通知笔记

--- docs/Tencent/微信公众平台/服务号/功能点/index.js
+++ docs/Tencent/微信公众平台/服务号/功能点/index.js
@@ -1,7 +1,8 @@
 const recommendedAttentionAfterPayment = require('./支付后推荐关注');
 const templateMessage = require('./模板消息');
+const subscribeNotify = require('./订阅通知');
 
 module.exports = {
   title: '功能点',
-  children: [recommendedAttentionAfterPayment, templateMessage]
+  children: [recommendedAttentionAfterPayment, templateMessage, subscribeNotify]
 };
