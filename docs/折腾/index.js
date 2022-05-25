bc811b2e0ade33bd18ea7d5ef2e1b4e010f7f09d
Iric <iricbing@gmail.com>
2022-05-25 12:16:52 +0800

    增加海思机顶盒部分笔记

--- docs/折腾/index.js
+++ docs/折腾/index.js
@@ -1,9 +1,10 @@
 const openwrt = require('./OpenWrt');
 const homeNetwork = require('./家庭网络');
 const octopusPlanet = require('./章鱼星球');
+const histb = require('./海思机顶盒');
 
 module.exports = {
   title: '折腾',
   path: '/折腾/',
-  children: [openwrt, homeNetwork, octopusPlanet]
+  children: [openwrt, homeNetwork, octopusPlanet, histb]
 };
---
+++ docs/折腾/海思机顶盒/index.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  title: '海思机顶盒',
+  path: '/折腾/海思机顶盒/',
+}
