b629ef9c546b0b6fb7cd683709f3e73112d0b7da
Iric <iricbing@gmail.com>
2022-06-02 17:53:49 +0800

    增加4G驱动

---
+++ docs/Linux/Ubuntu/实践积累/4G驱动/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: '4G 驱动',
+  path: '/Linux/Ubuntu/实践积累/4GDriver/'
+};
--- docs/Linux/Ubuntu/实践积累/index.js
+++ docs/Linux/Ubuntu/实践积累/index.js
@@ -1,6 +1,7 @@
-const systemUpgrade = require('./系统升级')
+const systemUpgrade = require('./系统升级');
+const catMDriver = require('./4G驱动');
 
 module.exports = {
   title: '实践积累',
-  children:[systemUpgrade]
+  children: [systemUpgrade, catMDriver]
 };
