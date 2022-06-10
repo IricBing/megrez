a38fc4df9c6338f8db7b3f35e2dfd997793060d6
Iric <iricbing@gmail.com>
2022-06-10 14:41:41 +0800

    增加章鱼星球armbian释放外置存储资源笔记

--- docs/折腾/章鱼星球/Armbian/index.js
+++ docs/折腾/章鱼星球/Armbian/index.js
@@ -1,7 +1,8 @@
 const systemConfig = require('./系统配置');
+const releaseOutStorage = require('./释放外置存储资源');
 
 module.exports = {
   title: 'Armbian',
   path: '/折腾/章鱼星球/Armbian/',
-  children: [systemConfig]
+  children: [systemConfig, releaseOutStorage]
 };
---
+++ docs/折腾/章鱼星球/Armbian/释放外置存储资源/index.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  title: '释放外置存储资源',
+  path: '/折腾/章鱼星球/Armbian/释放外置存储资源/',
+}
