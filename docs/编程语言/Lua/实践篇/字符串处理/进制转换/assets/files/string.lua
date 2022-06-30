ae044ca9fcb5db48d15f98cb1734bd8c4deb9528
Iric <iricbing@gmail.com>
2022-06-30 12:05:09 +0800

    更新Lua字符串处理笔记

--- docs/编程语言/Lua/实践篇/字符串处理/进制转换/assets/files/string.lua
+++ docs/编程语言/Lua/实践篇/字符串处理/进制转换/assets/files/string.lua
@@ -3,7 +3,7 @@
 --- @param separator string | nil 分隔符
 --- @return string 十六进制显示字符串
 function string.toHex(str, separator)
-    if (type(str) ~= "string") then return nil, "Invalid params type" end
+    if (type(str) ~= "string") then return nil, "string.toHex invalid params type" end
 
     local buffer = {}
     for index = 1, str:len() do
@@ -17,15 +17,15 @@ end
 --- @param hex string 十六进制显示的字符串
 --- @return string 标准字符串
 function string.fromHex(hex)
-    if (type(hex) ~= "string") then return nil, "Invalid params type" end
+    if (type(hex) ~= "string") then return nil, "string.fromHex invalid params type" end
     -- 滤掉分隔符
     hex = hex:gsub("[%s%p]", ""):upper()
     -- 检查内容是否合法
     if (hex:find("[^0-9A-Fa-f]") ~= nil) then
-        return nil, "Invalid params content"
+        return nil, "string.fromHex invalid params content"
     end
     -- 检查字符串长度
-    if (hex:len() % 2 ~= 0) then return nil, "Invalid params length" end
+    if (hex:len() % 2 ~= 0) then return nil, "string.fromHex invalid params length" end
     -- 拼接字符串
     local buffer = {}
     for index = 1, hex:len(), 2 do
