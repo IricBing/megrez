ad4fb0bb48e7488394ccbbdc3f7ecf66f9cc69ed
Iric <iricbing@gmail.com>
2022-07-01 16:42:53 +0800

    更新字符串进制转换处理笔记

--- docs/编程语言/Lua/实践篇/字符串处理/进制转换/assets/files/string.lua
+++ docs/编程语言/Lua/实践篇/字符串处理/进制转换/assets/files/string.lua
@@ -3,7 +3,7 @@
 --- @param separator string | nil 分隔符
 --- @return string 十六进制显示字符串
 function string.toHex(str, separator)
-    if (type(str) ~= "string") then return nil, "string.toHex invalid params type" end
+    if (type(str) ~= "string") then error("string.toHex invalid params type") end
 
     local buffer = {}
     for index = 1, str:len() do
@@ -17,20 +17,17 @@ end
 --- @param hex string 十六进制显示的字符串
 --- @return string 标准字符串
 function string.fromHex(hex)
-    if (type(hex) ~= "string") then return nil, "string.fromHex invalid params type" end
+    if (type(hex) ~= "string") then error("string.fromHex invalid params type") end
     -- 滤掉分隔符
     hex = hex:gsub("[%s%p]", ""):upper()
     -- 检查内容是否合法
-    if (hex:find("[^0-9A-Fa-f]") ~= nil) then
-        return nil, "string.fromHex invalid params content"
-    end
+    if (hex:find("[^0-9A-Fa-f]") ~= nil) then error("string.fromHex invalid params content") end
     -- 检查字符串长度
-    if (hex:len() % 2 ~= 0) then return nil, "string.fromHex invalid params length" end
+    if (hex:len() % 2 ~= 0) then error("string.fromHex invalid params length") end
     -- 拼接字符串
     local buffer = {}
     for index = 1, hex:len(), 2 do
-        table.insert(buffer,
-                     string.char(tonumber(hex:sub(index, index + 1), 16)))
+        table.insert(buffer, string.char(tonumber(hex:sub(index, index + 1), 16)))
     end
 
     return table.concat(buffer)
