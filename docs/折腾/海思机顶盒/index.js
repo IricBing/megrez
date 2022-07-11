59ac73f5589a25919ab77ccf9fe6446ab5a3a372
Iric <iricbing@gmail.com>
2022-07-11 15:29:20 +0800

    增加海思机顶盒CM301部分笔记

--- docs/折腾/海思机顶盒/index.js
+++ docs/折腾/海思机顶盒/index.js
@@ -1,5 +1,7 @@
+const deviceInfo = require('./设备信息');
 
 module.exports = {
   title: '海思机顶盒',
   path: '/折腾/海思机顶盒/',
-}
+  children: [deviceInfo]
+};
---
+++ docs/折腾/海思机顶盒/设备信息/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: '设备信息',
+  path: '/折腾/海思机顶盒/设备信息/'
+};
