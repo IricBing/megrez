e73e481a4a911af0d1a73fa54b4268d07d3c1fb5
Iric <iricbing@gmail.com>
2022-07-26 22:29:09 +0800

    增加C语言for循环笔记

---
+++ docs/编程语言/C/基础篇/语法基础/循环/for/README.md
@@ -0,0 +1,14 @@
+# for
+
+## 语法
+
+```c
+for (init; condition; increment)
+{
+   statement(s);
+}
+```
+
+## 流程图
+
+[循环流程图](./assets/images/循环流程.drawio ':include :type=code')
--- docs/编程语言/C/基础篇/语法基础/循环/index.js
+++ docs/编程语言/C/基础篇/语法基础/循环/index.js
@@ -5,6 +5,14 @@ module.exports = {
     {
       title: 'while 循环',
       path: '/编程语言/C/基础篇/语法基础/循环/while/'
+    },
+    {
+      title: 'for 循环',
+      path: '/编程语言/C/基础篇/语法基础/循环/for/'
+    },
+    {
+      title: 'do...while 循环',
+      path: '/编程语言/C/基础篇/语法基础/循环/dowhile/'
     }
   ]
 };
